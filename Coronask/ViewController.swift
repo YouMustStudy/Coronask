116caa7be8640b29977fd0d6ff0d13207bd1d618
kpugame <kpugame@kpugameui-MacBook-Pro-5.local>
2020-05-24 21:58:10 +0900

    Initial Commit

---
+++ Coronask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coronask
+//
+//  Created by kpugame on 2020/05/24.
+//  Copyright © 2020 Gurnwoo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
