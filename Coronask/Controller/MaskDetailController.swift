e3a35f87ff908019b348054432690a83cf407e1c
kpugame <kpugame@kpugameui-MacBook-Pro-5.local>
2020-06-06 20:43:42 +0900

    마스크 판매현황 - 검색결과에서 셀 클릭 시 세부정보 뷰로 이동 구현

--- Coronask/Controller/MaskDetailController.swift
+++ Coronask/Controller/MaskDetailController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MaskDetailController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    var store_info: STORE_INFO = STORE_INFO(addr: "", code: "", created_at: "", lat: 0.0, lng: 0.0, name: "", remain_stat: "", stock_at: "", type: "")
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,26 +22,36 @@ class MaskDetailController: UIViewController, UITableViewDataSource, UITableView
         return retval
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 5
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.value2, reuseIdentifier: nil)
-        switch (indexPath.row)
-        {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: nil)
+        switch (indexPath.row) {
         case 0:
             cell.textLabel?.text = "이름"
-            cell.detailTextLabel?.text = "더미약국"
-            break
-        case 1:
-            cell.textLabel?.text = "재고현황"
-            cell.detailTextLabel?.text = "~~개"
-            break
-        case 2:
-            cell.textLabel?.text = "주소"
-            cell.detailTextLabel?.text = "~~"
+            cell.detailTextLabel?.text = store_info.name
             break
             
-        default: break
+        case 1:
+            cell.textLabel?.text = "주소"
+            cell.detailTextLabel?.text = store_info.addr
+            break
+        case 2:
+            cell.textLabel?.text = "재고상황"
+            cell.detailTextLabel?.text = (store_info.remain_stat == nil) ? "정보없음" : store_info.remain_stat
+            break
+            
+        case 3:
+            cell.textLabel?.text = "입고시간"
+            cell.detailTextLabel?.text = (store_info.stock_at == nil) ? "정보없음" : store_info.stock_at
+            break
+            
+        case 4:
+            cell.textLabel?.text = "갱신시간"
+            cell.detailTextLabel?.text = (store_info.created_at == nil) ? "정보없음" : store_info.created_at
+            break
+        default:
+            break
         }
         return cell
     }
